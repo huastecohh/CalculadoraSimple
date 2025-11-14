15d885d5b498f3ea86df642b559ca0589bb90f03
Salvador Perez Zamoran <143537025+huastecohh@users.noreply.github.com>
2025-11-14 07:39:35 -0600

    PRIMER COMMIT

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author anima
+ */
+public class Mavenproject1 { //PRIMER MODIFICACION HACIENDO UN COMMIT
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
